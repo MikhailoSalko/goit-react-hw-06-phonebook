8f97dca69bdfa2af5c2618ff1b402127957c5405
Mikhailo Salko <slipknotmisha@gmail.com>
2023-06-11 14:08:26 +0300

    add filterSlice and rename persistor

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
-import { persistedStore, store } from 'redux/store';
+import { persistor, store } from 'redux/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistedStore}>
+    <PersistGate loading={null} persistor={persistor}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
